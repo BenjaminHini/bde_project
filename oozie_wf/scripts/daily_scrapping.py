9301862bd9ab44d9ae0a9e53b411bb599f2afb9b
BenjaminHini <benjamin.hini@edu.ece.fr>
2022-12-30 11:18:36 +0100

    oozie testing with another script

--- oozie_wf/scripts/daily_scrapping.py
+++ oozie_wf/scripts/daily_scrapping.py
@@ -4,43 +4,47 @@ import pandas as pd
 import numpy as np
 from datetime import date
 
-ZODIAC_SIGNS = {
-    "Aries": 1,
-    "Taurus": 2,
-    "Gemini": 3,
-    "Cancer": 4,
-    "Leo": 5,
-    "Virgo": 6,
-    "Libra": 7,
-    "Scorpio": 8,
-    "Sagittarius": 9,
-    "Capricorn": 10,
-    "Aquarius": 11,
-    "Pisces": 12
-}
+# ZODIAC_SIGNS = {
+#     "Aries": 1,
+#     "Taurus": 2,
+#     "Gemini": 3,
+#     "Cancer": 4,
+#     "Leo": 5,
+#     "Virgo": 6,
+#     "Libra": 7,
+#     "Scorpio": 8,
+#     "Sagittarius": 9,
+#     "Capricorn": 10,
+#     "Aquarius": 11,
+#     "Pisces": 12
+# }
 
-def get_horoscope_by_day(zodiac_sign: int, day: str):
-    if not "-" in day:
-        res = requests.get(f"https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-{day}.aspx?sign={zodiac_sign}")
-    else:
-        day = day.replace("-", "")
-        res = requests.get(f"https://www.horoscope.com/us/horoscopes/general/horoscope-archive.aspx?sign={zodiac_sign}&laDate={day}")
-    soup = BeautifulSoup(res.content, 'html.parser')
-    data = soup.find('div', attrs={'class': 'main-horoscope'})
-    return data.p.text
+# def get_horoscope_by_day(zodiac_sign: int, day: str):
+#     if not "-" in day:
+#         res = requests.get(f"https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-{day}.aspx?sign={zodiac_sign}")
+#     else:
+#         day = day.replace("-", "")
+#         res = requests.get(f"https://www.horoscope.com/us/horoscopes/general/horoscope-archive.aspx?sign={zodiac_sign}&laDate={day}")
+#     soup = BeautifulSoup(res.content, 'html.parser')
+#     data = soup.find('div', attrs={'class': 'main-horoscope'})
+#     return data.p.text
 
-today = date.today()#for the database continuity
-today_formated = "today"#for the http request
-dates = 12* [today]
-signs = []
-descritpions = []
-for sign in ZODIAC_SIGNS.values():
-    signs.append(str(sign))
-    descritpions.append(get_horoscope_by_day(sign, today_formated))
+# today = date.today()#for the database continuity
+# today_formated = "today"#for the http request
+# dates = 12* [today]
+# signs = []
+# descritpions = []
+# for sign in ZODIAC_SIGNS.values():
+#     signs.append(str(sign))
+#     descritpions.append(get_horoscope_by_day(sign, today_formated))
 
-df = pd.DataFrame([dates, signs, descritpions]).T
-df.columns = ['dates','signs','description']
-df.to_json('daily_scrap.json')
-df.to_csv('dail_scrap.csv')
+# df = pd.DataFrame([dates, signs, descritpions]).T
+# df.columns = ['dates','signs','description']
+# df.to_json('daily_scrap.json')
+# df.to_csv('dail_scrap.csv')
 
+
+f = open("data.txt", "w")
+f.write("Hello")
+f.close()
 
